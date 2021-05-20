bf10c0df90fc5b8ec053403c0367320a3172438f
Jonathan Heugh <mrjbheugh@gmail.com>
2021-05-20 09:01:34 +0200

    Worked on level 1

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>High Scores Per Country</h1>
+        </header>
+    );
+}
+
+export default Header;
